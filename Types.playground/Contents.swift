fa8a35a0200f0573fe9fa5d0dc347da5b7ecf7bf
Blake Mazzaferro <blake.mazzaferro@gmail.com>
2019-01-22 20:39:54 -0500

    Complete Through 2.11!

---
+++ Types.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var myInt = 30000
+var myDouble = 30000.0
+var myBool = true
+
+var greeting = "Hello, "
+var name = "John"
+var name2 = "john"
+
+var greetingString = greeting + name
+
+print(greetingString)
+
+//var kilometers: Double = 5
+var kilometers = 10
+var kmInMiles = 1.609
+
+var howMuchIRan = Double(kilometers)/kmInMiles
+
+var isEqual = name == name2
+
+var myWeight = 170
+var percentGoal = 0.12
+
+var weightToLose = Double(myWeight)*percentGoal
+
+var weightMessage = "My weight is currently " + String(myWeight)
+print(weightMessage)
+
+var oneEighth = 1.0/8.0
+var newWeightLossGoal = Double(myWeight) * oneEighth
